1febdf3e4f07aed5fc9c9e1c02d3c1bb30dc40b4
John Benedict Bibit <johnbenedictbibit.bibit@students.isatu.edu.ph>
2024-08-28 01:48:33 +0000

    rigor

---
+++ rigor/druglist.R
@@ -0,0 +1,5 @@
+drugs <- list("Alcohol", "Ayahuasca", "Cannabis (Marijuana/Pot/Weed)","Central Nervous System Depressants (Benzos)","Cocaine (Coke/Crack)","Fentanyl","GHB","Hallucinogens","Tobacco/Nicotine and Vaping")
+
+for (x in drugs) {
+  print(x)
+}
