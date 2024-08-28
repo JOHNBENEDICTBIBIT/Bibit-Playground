1598566ca4810fc40c66886652c072eab51cb452
John Benedict Bibit <johnbenedictbibit.bibit@students.isatu.edu.ph>
2024-08-28 01:37:52 +0000

    drug list

--- Sample/sample.R
+++ Sample/sample.R
@@ -1,3 +1,5 @@
-100 + 99
-
-print("Shabu")
+drugs <- list("Alcohol", "Ayahuasca", "Cannabis (Marijuana/Pot/Weed)","Central Nervous System Depressants (Benzos)","Cocaine (Coke/Crack)","Fentanyl","GHB","Hallucinogens","Tobacco/Nicotine and Vaping")
+              
+for (x in drugs) {
+  print(x)
+}
